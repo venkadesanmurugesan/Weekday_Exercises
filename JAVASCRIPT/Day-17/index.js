dcd2d42008a1e23f919bbd0fb15b048c7f1a0dfa
venkadesanmurugesan <69321204+venkadesanmurugesan@users.noreply.github.com>
2021-04-07 21:55:35 +0530

    initial commit ("apr 7")

---
+++ JAVASCRIPT/Day-17/index.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = "Hello World";
+var person={
+    name:"venky",age:18,degree:"B.SC CT"
+};
+
+var student = ['venky','siva','kathi','jegan'];
+
+// console.log(typeof(a));
+// console.log(b);
+console.log(person);
+console.log(person.name);
+// console.log("object type"+typeof(person.age));
+console.log(typeof(student));
+console.log(student[2]);
+console.log(student.length)
+
+
---
+++ JAVASCRIPT/Day-17/operations.js
@@ -0,0 +1,58 @@
+// var a = 30;
+// var b = 50;
+// var sum;
+// sum = a+b;
+// console.log(sum);
+
+// var diff;
+// diff = b-a;
+// console.log(diff);
+
+// var product;
+// product = a*b;
+// console.log(product);
+
+// var div;
+// div = a/b;
+// console.log(div);
+
+// var mod;
+// mod = b%b;
+// console.log(mod);
+
+// // CONDITION OPERATORS
+
+// var a = 20;
+//  if(a==20){
+//      console.log('YES');
+//  }
+//  else{
+//      console.log('NO');
+//  }
+
+//  var a = 10;
+//  if(a==20){
+//      console.log('YES');
+//  }
+//  else{
+//      console.log('NO');
+//  }
+
+
+// FOR LOOP
+
+// var a = 10;
+
+// for(var i=1; i<=a; i++){
+//     console.log(i)
+// }
+
+var a = 20;
+var b = 20;
+
+if (a ==20 || a>20){
+    console.log("YES");
+}
+else{
+    console.log('no')
+}
